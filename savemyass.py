d7a68e18a6e86c7821cddb3a88b25811fe98054e
Дмитрий Няшкин <admin@fdcore.com>
2014-11-20 07:49:40 +0300

    fixed args and check config file

--- savemyass.py
+++ savemyass.py
@@ -16,42 +16,42 @@ folder = 'tmp/'+date
 
 def parse_command_line(line):
   global folder
-  
+
   lines = line.split(' ')
   cmd = lines[0]
   del lines[0]
 
   command = " ".join(lines)
-  
+
   if cmd == 'run':
     run(command)
-    
+
   if cmd == 'cd':
     cd(command)
-    
+
   if cmd == 'sudo':
     sudo(command)
-        
+
   if cmd == 'get':
     if os.path.exists(folder) is False:
       os.makedirs(folder)
-      
+
     get(command, folder)
-  
-# print yaml.dump(y, default_flow_style=False)
 
 def main(argv):
    global folder
-   
+
    inputfile = ''
    outputfile = ''
    directory = ''
-   
+
    try:
-      opts, args = getopt.getopt(argv,"hi:do:",["ifile=","ofile="])
+      opts, args = getopt.getopt(argv,"h:i:d:o:")
+      print opts 
    except getopt.GetoptError:
       print 'savemyass.py -i <inputfile> -d <directory> -o <outputfile>'
       sys.exit(2)
+
    for opt, arg in opts:
       if opt == '-h':
          print 'savemyass.py -i <inputfile> -o <outputfile>'
@@ -62,15 +62,16 @@ def main(argv):
          outputfile = arg
       elif opt in ("-d", "--directory"):
          directory = arg
-         
+
    if inputfile == None and directory == None:
      print 'savemyass.py -i <inputfile> -d <directory> -o <outputfile> '
      return False
-   
+
    if outputfile == None:
      outputfile = ''
-     
-   
+
+   return
+
    if directory:
       directory_list = os.listdir(directory)
       for dl in directory_list:
@@ -78,23 +79,26 @@ def main(argv):
          y = yaml.load(f)
          env.host_string = "%s@%s:%s" % (y['login'], y['host'], y['port'])
          env.password = y['password']
-         
+
          date = datetime.now().strftime("%Y-%m-%d")
          folder = "%s/%s/%s" % (outputfile, y['host'], date)
-         
+
          for c in y['commands']:
             parse_command_line(c)
    else:
-        f = open(inputfile, 'r')
-        y = yaml.load(f)
-        env.host_string = "%s@%s:%s" % (y['login'], y['host'], y['port'])
-        env.password = y['password']
+        if os.path.exists(inputfile):
+          f = open(inputfile, 'r')
+          y = yaml.load(f)
+          env.host_string = "%s@%s:%s" % (y['login'], y['host'], y['port'])
+          env.password = y['password']
 
-        date = datetime.now().strftime("%Y-%m-%d")
-        folder = "%s/%s/%s" % (outputfile, y['host'], date)
+          date = datetime.now().strftime("%Y-%m-%d")
+          folder = "%s/%s/%s" % (outputfile, y['host'], date)
 
-        for c in y['commands']:
-            parse_command_line(c)
-   
+          for c in y['commands']:
+              parse_command_line(c)
+        else:
+          print 'config file not found'
+
 if __name__ == "__main__":
    main(sys.argv[1:])
