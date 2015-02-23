bfb551c6ba622f777f3ec859a301b8b2eb9e9202
Дмитрий Няшкин <admin@fdcore.com>
2015-02-23 10:12:47 +0300

    v 0.2
    add support webdav

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Install
 requirements:
   - fabric
   - PyYAML
+  - easywebdav
   
   pip install -r requirements.txt
 
@@ -39,6 +40,22 @@ port: 22
 commands: # commands list
   - run ls  # execute command
   - get /var/log/nginx/errors.log # get file to output directory
+  - local ls # local execure command
+  - set errors.log # upload localfile to webdav
+```
+
+WebDav
+------
+
+Script support upload backup files to any WebDav.
+In yaml confid need set params in "webdav" section.
+
+example
+```yaml
+webdav:
+  host: webdav.your-domain.com
+  username: mylogin
+  password: 'passw0rd'
 ```
 
 Full Example
@@ -49,12 +66,26 @@ host: 123.123.123.123
 login: root
 password: myg00dpassw0rd
 port: 22
+webdav:
+  host: dav.box.com
+  path: '/dav/backup'
+  protocol: https
+  username: admin@test.com
+  password: 'myg00dpassw0rd'
 commands:
     - run tar cfvz /tmp/backup.tar.gz /var/www/
     - get /tmp/backup.tar.gz
-    - run mysqldump -u root --password 'mysqlpassw0rd' --all-databases > /tmp/all-database.sql; echo 'y'
+    - run mysqldump -u root --password=mysqlpassw0rd --all-databases > /tmp/all-database.sql; echo 'y'
     - run gzip /tmp/all-database.sql
     - get /tmp/all-database.sql.gz
     - run rm /tmp/all-database.sql.gz
     - run rm /tmp/backup.tar.gz
+    - set backup.tar.gz
 ```
+
+Changelog
+---------
+0.2
+  - add support upload to webdav
+  - fixes `getopt`
+  
--- savemyass.py
+++ savemyass.py
@@ -2,56 +2,124 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Dmitriy Nyashkin'
+__version__ = '0.2'
 
-from fabric.api import env, run, get, cd, sudo
+from fabric.api import env, run, get, cd, sudo, local
 from datetime import datetime
 import os
 import yaml
 import sys, getopt
+import easywebdav
 
 date = datetime.now().strftime("%Y-%m-%d")
 time = datetime.now().strftime("%H-%M-%S")
 
 folder = 'tmp/'+date
+root_host_dir = "tmp/"
+config = {}
 
+def download_file(filename):
+    global folder, root_host_dir
+    
+    get(filename, folder) # скачиваем файл
+
+    _filename = filename.split('/')[-1]
+    _size_path = root_host_dir+_filename+".size" 
+    last_file_size = ''
+    
+    # если есть файл с данными размера файла
+    if os.path.exists(_size_path):
+        last_file_size = open(_size_path, 'rb').read()
+    
+    # текущий размер файла
+    current_file_size = str(os.path.getsize(folder+'/'+_filename))
+    
+    # проверка на размер прошлого бекапа + защита от удаления если прошлый бекап был сегодня
+    if last_file_size == current_file_size:
+        print 'File deleted ' + folder+'/'+_filename
+        os.remove(folder+'/'+_filename)
+    else:
+        _file = open(_size_path, "w")
+        _file.write(current_file_size)
+        _file.close()
+
+# загружаем файл, пока что по WebDav
+def upload_file(filename):
+    global config, folder
+    
+    filename = folder+"/"+filename
+    
+    if 'webdav' not in config:
+        return False
+        
+    if os.path.exists(filename) is False:
+        return False
+        
+    if 'connected' not in config:
+        webdav = easywebdav.connect(**config['webdav'])
+        config['connected'] = webdav
+    else:
+        webdav = config['connected']
+        
+    dirname = os.path.dirname(filename)
+
+    if webdav.exists(dirname) is False:
+        webdav.mkdirs(dirname)
+
+    webdav.upload(filename, filename)
+
+# Парсим yaml файл
+def yaml_parse(filename):
+    y = False
+    
+    if os.path.exists(filename):
+        f = open(filename, 'r')
+        y = yaml.load(f)
+    
+    return y
+
+def set_paths(outdir, hostname):
+    global folder, root_host_dir
+    
+    date = datetime.now().strftime("%Y-%m-%d") # or %Y-%m-%d %H-%M
+    folder = "%s/%s/%s" % (outdir, hostname, date)
+    root_host_dir = "%s/%s/" % (outdir, hostname)
+    
+    if os.path.exists(folder) is False:
+        os.makedirs(folder)
+    
+    
+# команды для выполнения
 def parse_command_line(line):
-  global folder
-
+  global folder, root_host_dir
+  
   lines = line.split(' ')
   cmd = lines[0]
   del lines[0]
 
   command = " ".join(lines)
-
-  if cmd == 'run':
-    run(command)
-
-  if cmd == 'cd':
-    cd(command)
-
-  if cmd == 'sudo':
-    sudo(command)
-
-  if cmd == 'get':
-    if os.path.exists(folder) is False:
-      os.makedirs(folder)
-
-    get(command, folder)
+  
+  if cmd == 'run': run(command)
+  if cmd == 'cd': cd(command)
+  if cmd == 'local': local(command)
+  if cmd == 'sudo': sudo(command)
+  if cmd == 'get' or cmd == 'download': download_file(command) # передан путь к файлу
+  if cmd == 'set' or cmd == 'upload': upload_file(command)
 
 def main(argv):
-   global folder
-
+   global folder, root_host_dir, config
+   
    inputfile = ''
    outputfile = ''
    directory = ''
-
+   
    try:
       opts, args = getopt.getopt(argv,"h:i:d:o:")
-      print opts 
+
    except getopt.GetoptError:
       print 'savemyass.py -i <inputfile> -d <directory> -o <outputfile>'
       sys.exit(2)
-
+    
    for opt, arg in opts:
       if opt == '-h':
          print 'savemyass.py -i <inputfile> -o <outputfile>'
@@ -62,43 +130,43 @@ def main(argv):
          outputfile = arg
       elif opt in ("-d", "--directory"):
          directory = arg
-
+         
    if inputfile == None and directory == None:
      print 'savemyass.py -i <inputfile> -d <directory> -o <outputfile> '
      return False
-
+   
    if outputfile == None:
      outputfile = ''
-
-   return
-
+        
    if directory:
       directory_list = os.listdir(directory)
       for dl in directory_list:
-         f = open(directory+'/'+dl, 'r')
-         y = yaml.load(f)
+         y = yaml_parse(directory+'/'+dl)
+         
+         if y is False:
+             continue
+             
          env.host_string = "%s@%s:%s" % (y['login'], y['host'], y['port'])
          env.password = y['password']
-
-         date = datetime.now().strftime("%Y-%m-%d")
-         folder = "%s/%s/%s" % (outputfile, y['host'], date)
-
+         config = y
+         set_paths(outputfile, y['host'])
+         
          for c in y['commands']:
             parse_command_line(c)
    else:
-        if os.path.exists(inputfile):
-          f = open(inputfile, 'r')
-          y = yaml.load(f)
+        y = yaml_parse(inputfile)
+        
+        if y:
           env.host_string = "%s@%s:%s" % (y['login'], y['host'], y['port'])
           env.password = y['password']
-
-          date = datetime.now().strftime("%Y-%m-%d")
-          folder = "%s/%s/%s" % (outputfile, y['host'], date)
-
+          
+          set_paths(outputfile, y['host'])
+          config = y
+          
           for c in y['commands']:
               parse_command_line(c)
         else:
           print 'config file not found'
-
+          
 if __name__ == "__main__":
    main(sys.argv[1:])
